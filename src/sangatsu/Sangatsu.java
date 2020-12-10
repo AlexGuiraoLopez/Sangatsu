26a66b54e58b9398cb8bf43b8fa408ae03833232
Floydeku <notxa_alex@hotmail.com>
2020-12-10 21:54:31 +0100

    he creado el proyecto y he añadido el dibujo del tres en raya. autor: tu nombre

---
+++ src/sangatsu/Sangatsu.java
@@ -0,0 +1,20 @@
+package sangatsu;
+
+/**
+ *
+ * @author Alex Guirao López <aguiraol2021@cepnet.net>
+ */
+public class Sangatsu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        |1|2|3|
+        |4|X|6|
+        |7|8|9|
+        */
+    }
+    
+}
